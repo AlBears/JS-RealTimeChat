8a7f8a96c878369c7b68c3b70d0e6bfe1e189dd3
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-09-11 15:36:56 +0300

    Express setup

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 "use strict";
 
-var http = require("http");
-var express = require("express");
+var http = require("http"),
+	express = require("express");
 
 const app = express();
+app.set("view engine", "jade");
+
+app.use((request, response, next) => {
+	console.log("In middleware 1");
+	//response.write("HEADER");
+	next();
+	console.log("Out middleware 1");
+});
+
+app.use(express.static("./public"));
+
+
+app.use((request, response, next) => {
+	console.log("--In middleware 2");
+	next();
+	console.log("---Out middleware 2");
+});
 
 app.get("/", (request, response) => {
-  response.end("Hello, World!");
+	response.end("Hello, World!");
+	console.log("In Handler");
+});
+
+app.get("/home", (request, response) => {
+	response.render("index", {title: "TITLE!"});
 });
 
 const server = new http.Server(app);
 
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+	console.log(`Server started on port ${port}`);
 });
